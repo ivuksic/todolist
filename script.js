df1d12ab6a2bf89212cce3140dea9f57c7cd9748
Ivan Vuksic <ivuksic@gmail.com>
2024-01-14 11:40:57 +0100

    save function, adding, removing

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask() {
   if (inputBox.value === "") {
     alert("You must write something!");
@@ -12,4 +13,23 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
 }
